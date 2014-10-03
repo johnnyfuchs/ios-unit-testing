77bc3268677c642e8ba129fed0c6ebd5ee8e56be
johnnyfuchs <johnnyfuchs@gmail.com>
2014-10-03 01:17:31 -0700

    Initial Commit

---
+++ login/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  login
+//
+//  Created by Johnny Sparks on 10/3/14.
+//  Copyright (c) 2014 beergrammer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ login/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  login
+//
+//  Created by Johnny Sparks on 10/3/14.
+//  Copyright (c) 2014 beergrammer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
