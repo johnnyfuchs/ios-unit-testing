13824f5effdd2d593a31b818f1dec98e9e61429e
johnnyfuchs <johnnyfuchs@gmail.com>
2014-10-07 09:28:09 -0700

    Adding gcov_flush()... still not working

--- login/AppDelegate.m
+++ login/AppDelegate.m
@@ -9,6 +9,8 @@
 #import "AppDelegate.h"
 #import "ViewController.h"
 
+extern void __gcov_flush();
+
 @interface AppDelegate ()
 
 @end
@@ -32,6 +34,8 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
+    __gcov_flush();
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -44,6 +48,7 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    __gcov_flush();
 }
 
 @end
